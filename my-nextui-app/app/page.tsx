d5be25ee21490e606bdf1b502c1505e4064339ca
shinsangwoo <ssw2570@naver.com>
2024-09-01 13:24:08 +0900

    nextui 맛보기

---
+++ my-nextui-app/app/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+import {Calendar} from "@nextui-org/calendar";
+import {parseDate} from '@internationalized/date';
+import React from "react";
+import {DateRangePicker} from "@nextui-org/react";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+  let [value, setValue] = React.useState(parseDate("2024-03-07"));
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-xl text-center justify-center">
+        <h1 className={title()}>Make&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+        <br />
+        <h1 className={title()}>
+          websites regardless of your design experience.
+        </h1>
+        <h2 className={subtitle({ class: "mt-4" })}>
+          Beautiful, fast and modern React UI library.
+        </h2>
+      </div>
+
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+          href={siteConfig.links.docs}
+        >
+          Documentation
+        </Link>
+        <Link
+          isExternal
+          className={buttonStyles({ variant: "bordered", radius: "full" })}
+          href={siteConfig.links.github}
+        >
+          <GithubIcon size={20} />
+          GitHub
+        </Link>
+      </div>
+<div>
+      <Calendar 
+      aria-label="Date (Controlled)" 
+      value={value} 
+      onChange={setValue} 
+    />
+</div>
+<DateRangePicker 
+      label="기간" 
+      className="max-w-xs" 
+    />
+      <div className="mt-8">
+        <Snippet hideCopyButton hideSymbol variant="bordered">
+          <span>
+            Get started by editing <Code color="primary">app/page.tsx</Code>
+          </span>
+        </Snippet>
+      </div>
+    </section>
+  );
+}
